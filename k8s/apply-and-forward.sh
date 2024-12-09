4a0ac50958f66074bb71af22fe395dc5d96984aa
icqun <jln.schludermann@gmail.com>
2024-12-09 21:18:02 +0100

    fix: apply-and-forward script

--- k8s/apply-and-forward.sh
+++ k8s/apply-and-forward.sh
@@ -4,15 +4,26 @@ set -e
 
 _dir=$(dirname $0)
 
+echo -e "\napplying deployments..."
 kubectl apply -f ${_dir}/appsrv-classic.yaml
 kubectl apply -f ${_dir}/appsrv-mutiny.yaml
 kubectl apply -f ${_dir}/postgres.yaml
 
+echo -e "\nrestarting deployments..."
+kubectl apply -f ${_dir}/appsrv-classic.yaml
 kubectl rollout restart deployment appsrv-classic
 kubectl rollout restart deployment appsrv-mutiny
 
+echo -e "\nwaiting for deployments to become available..."
+kubectl wait --for=condition=available --timeout=600s deployment/appsrv-classic
+kubectl wait --for=condition=available --timeout=600s deployment/appsrv-mutiny
+
+echo -e "\nwaiting for pods to come online..."
+kubectl wait --for=condition=ready --timeout=600s pod --all
+
+echo -e "\nforwarding ports...."
 kubectl port-forward deployment/appsrv-classic 8080:8080 & \
 kubectl port-forward deployment/appsrv-mutiny 8081:8081 &
 
-echo "Press CTRL-C to exit"
+echo -e "\nPress CTRL-C to exit"
 wait
