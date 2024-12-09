c76528477d43c3827fc9669018747f3946c02506
icqun <jln.schludermann@gmail.com>
2024-12-09 20:36:43 +0100

    fix: create tables in prod

--- k8s/apply-and-forward.sh
+++ k8s/apply-and-forward.sh
@@ -11,16 +11,8 @@ kubectl apply -f ${_dir}/postgres.yaml
 kubectl rollout restart deployment appsrv-classic
 kubectl rollout restart deployment appsrv-mutiny
 
-#
-#while : ; do
-#  containers=$(kubectl get pods --sort-by=.status.startTime | grep appsrv-classic | cut -d' ' -f1 | tac)
-#  c=$(echo $containers| cut --delimiter " " --fields 1)
-#  echo "Waiting for $c to come online"
-#  kubectl get pod $c | grep Running >/dev/null
-#  res=$?
-#
-#  [[ $res != 0 ]] || break
-#done
-#
-#echo "forwarding $c"
-#kubectl port-forward $c 8080:8080
+kubectl port-forward deployment/appsrv-classic 8080:8080 & \
+kubectl port-forward deployment/appsrv-mutiny 8081:8081 &
+
+echo "Press CTRL-C to exit"
+wait
